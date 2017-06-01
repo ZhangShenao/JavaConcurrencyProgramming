42fdbb3007ab7739772f718ba40ca06ab4ecde58
ZhangShenao <15201440436@163.com>
2017-06-02 07:44:14 +0800

    数据库连接池

---
+++ src/ch04/ConnectionDriver.java
@@ -0,0 +1,36 @@
+package ch04;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.sql.Connection;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 
+ * <p>Description:  数据库连接池驱动</p>
+ * @author ZhangShenao
+ * @date 2017年6月2日 上午7:14:18
+ */
+public class ConnectionDriver {
+	private static class DriverHandler implements InvocationHandler{
+		@Override
+		public Object invoke(Object proxy, Method method, Object[] args)
+				throws Throwable {
+			Object result = method.invoke(null, args);
+			if ("commit".equals(method.getName())){
+				TimeUnit.MILLISECONDS.sleep(100);
+			}
+			return result;
+		}
+		
+	}
+	
+	/**
+	 * 创建数据库连接
+	 */
+	public static final Connection getConnection(){
+		return (Connection) Proxy.newProxyInstance(ConnectionDriver.class.getClassLoader(), new Class[]{Connection.class}, 
+				new DriverHandler());
+	}
+}
---
+++ src/ch04/ConnectionPool.java
@@ -0,0 +1,77 @@
+package ch04;
+
+import java.sql.Connection;
+import java.util.LinkedList;
+
+/**
+ * 
+ * <p>Description:  数据库连接池</p>
+ * @author ZhangShenao
+ * @date 2017年6月2日 上午7:19:13
+ */
+public class ConnectionPool {
+	/**
+	 * 使用双端队列保存数据库连接
+	 */
+	private LinkedList<Connection> pool = new LinkedList<>();
+	
+	/**
+	 * 初始化数据库连接池
+	 * @param initCapacity 初始连接数,默认为10
+	 */
+	public ConnectionPool(int initCapacity){
+		if (initCapacity <= 0){
+			initCapacity = 10;
+		}
+		for (int i = 0;i < initCapacity;i++){
+			pool.addLast(ConnectionDriver.getConnection());
+		}
+	}
+	
+	/**
+	 * 释放数据库连接
+	 */
+	public void releaseConnection(Connection connection){
+		if (null != connection){
+			synchronized (pool) {
+				pool.addLast(connection);
+				
+				//释放连接后通知消费者,这样其他消费者就可以感知到连接池中已经归还了一个连接
+				pool.notifyAll();
+			}
+		}
+	}
+	
+	/**
+	 * 获取数据库连接池
+	 * @param mills 超时等待时间。如果在mills毫秒内仍未获取到连接,则返回null
+	 */
+	public Connection fetchConenction(long mills) throws InterruptedException{
+		synchronized (pool) {
+			//无超时等待
+			if (mills <= 0){
+				while (pool.isEmpty()){
+					pool.wait();
+				}
+				
+				return pool.removeFirst();
+			}
+				
+			//超时等待模式
+			long future = System.currentTimeMillis() + mills;
+			long remaining = mills;
+			
+			while (pool.isEmpty() && remaining > 0){
+				pool.wait(remaining);
+				remaining = future - System.currentTimeMillis();
+			}
+			
+			Connection connection = null;
+			if (!pool.isEmpty()){
+				connection = pool.removeFirst();
+			}
+			return connection;
+		}
+		
+	}
+}
